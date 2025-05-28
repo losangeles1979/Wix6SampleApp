25426281ace6f75d82cb86f6b96e555ea0f90710
losangeles1979 <ROCKETDYNE1979@GMAIL.COM>
2025-05-27 22:06:17 -0500

    Icons for the program shortcut, and the title bar of the form.

--- Program.cs
+++ Program.cs
@@ -16,6 +16,14 @@ namespace Wix6SampleApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            /*
+             To set the icon and title of the form that displays:
+               1. Double-click Form1.cs to bring up the Design view
+               2. Right-click on the form, select Properties
+               3. Go to Icon, navigate to the *.ico file (32x32 size)
+               4. Go to Text and set the form's title
+            */
             Application.Run(new Form1());
         }
     }
