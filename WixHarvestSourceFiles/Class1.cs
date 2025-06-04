97e037c29a1d0922762b601d09e645f6fc213b3c
losangeles1979 <ROCKETDYNE1979@GMAIL.COM>
2025-06-03 20:16:21 -0500

    Tried to get the new source file harvesting feature of Wix 6 to work.  It's not available.  Documenting my lessons learned for future reference.

---
+++ WixHarvestSourceFiles/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+/* THIS IS A DUMMY FILE THAT IS INSTALLED AND SUBSEQUENTLY COPIED
+   BY THE CustomCopyFiles.bat BATCH FILE, WHICH IS KICKED OFF
+   BY INCLUDING IT IN THE CHAIN OF BUNDLE.WXS. 
+*/
+
+/* IT IS ALSO HARVESTED ALONG WITH ALL OTHER FILES IN THIS DIRECTORY 
+   BY USING THE WIX HARVEST CLI LIKE THIS:
+   wix harvest directory -o HarvestedFiles.wxs -nologo -ag -var var.MySourceDir MyFiles
+*/
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
